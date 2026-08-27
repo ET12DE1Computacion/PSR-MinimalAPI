7388d0b9e949fbc44bd879aa7cfd2e6ece2dbe30
Diego-2810 <diegoemilianoalganaras@gmail.com>
2026-08-27 00:10:10 -0300

    Cambios-1

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,8 +3,13 @@ using Microsoft.Extensions.DependencyInjection;
 using Backend.Repositories;
 using Backend.Sockets;
 using Backend.Endpoints;
+using Scalar.AspNetCore; // <-- NUEVO 1: Importamos la librería de Scalar
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --- NUEVO 2: Configuramos el generador del documento técnico (OpenAPI) ---
+builder.Services.AddOpenApi(); 
+// --------------------------------------------------------------------------
 
 // Registro de repositorios y servicios de negocio
 builder.Services.AddSingleton<IPedidoRepository, InMemoryPedidoRepository>();
@@ -14,6 +19,11 @@ builder.Services.AddSingleton<SocketServerService>();
 builder.Services.AddHostedService(provider => provider.GetRequiredService<SocketServerService>());
 
 var app = builder.Build();
+
+// --- NUEVO 3: Encendemos los endpoints de la interfaz gráfica ---
+app.MapOpenApi(); // Crea un archivo .json oculto con la estructura de tu API
+app.MapScalarApiReference(); // Lee ese .json y te crea la página web visual
+// ----------------------------------------------------------------
 
 // Endpoint de prueba rápido
 app.MapGet("/", () => "API de Gestión de Pedidos - Servidor en línea.");
